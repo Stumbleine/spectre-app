121a82eee5054e36ad108bbff5a8d460dc96babd
cristhian <hillyellow42@gmail.com>
2021-10-18 18:29:09 -0400

    creacion componentes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  {path: 'home',component: HomeComponent},
+  {path: 'characters', component:CharactersComponent},
+  {path: 'locations', component:LocationsComponent},
+  {path: 'about-us', component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http' 
+import {RequestService} from '../app/services/request.service';
 
+//Componentes Material UI
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
+import {MatCardModule} from '@angular/material/card'
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSelectModule} from '@angular/material/select'
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatStepperModule} from '@angular/material/stepper'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatListModule } from '@angular/material/list';
+import {MatAutocompleteModule } from '@angular/material/autocomplete';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+
+//Componentes angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './components/card/card.component';
+import { SideBarComponent } from './componets/side-bar/side-bar.component';
+
 
 @NgModule({
   declarations: [
+    //declaracion de componentes angular
     AppComponent,
-    CardComponent
+    CardComponent,
+    SideBarComponent
   ],
   imports: [
+    //declaracion de acomponentes Material, modulos externos
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
